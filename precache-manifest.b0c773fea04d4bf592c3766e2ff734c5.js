20e2751798598ff369b7daa7dfb8061351631223
nitkum41 <nitkum41@users.noreply.github.com>
2021-08-21 10:27:28 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.b0c773fea04d4bf592c3766e2ff734c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39fe7e112ec90a51cfc96d8f78a2647",
+    "url": "https://nitkum41.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "3d02d212079e941c4800",
+    "url": "https://nitkum41.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "77f71dca84185c989c63",
+    "url": "https://nitkum41.github.io/task-management-frontend/static/js/2.d8cbb175.chunk.js"
+  },
+  {
+    "revision": "3d02d212079e941c4800",
+    "url": "https://nitkum41.github.io/task-management-frontend/static/js/main.fefa10d3.chunk.js"
+  },
+  {
+    "revision": "2819370c3cf0b63a2ed3",
+    "url": "https://nitkum41.github.io/task-management-frontend/static/js/runtime~main.8e46c36a.js"
+  }
+]);
